ffa6e8b59927a713bad599eb7d808dfd54d4d645
Devedse <davydavidse@gmail.com>
2019-05-18 23:16:39 +0200

    Improved unit tests

--- DeveCoolLib.Tests/TextFormatting/TableToTextPrinterFacts.cs
+++ DeveCoolLib.Tests/TextFormatting/TableToTextPrinterFacts.cs
@@ -14,6 +14,7 @@ namespace DeveCoolLib.Tests.TextFormatting
             //Arrange
             var data = new List<List<string>>();
             data.Add(new List<string>() { "", "First Name", "Last Name" });
+            data.Add(null);
             data.Add(new List<string>() { "1", "Heinz", "Dovenschmirtz" });
             data.Add(new List<string>() { "2", "Mickey", "Mouse" });
 
@@ -21,7 +22,7 @@ namespace DeveCoolLib.Tests.TextFormatting
             var result = TableToTextPrinter.TableToText(data);
 
             //Assert
-            var expected = $@"|   | First Name |   Last Name   |{Environment.NewLine}| 1 |   Heinz    | Dovenschmirtz |{Environment.NewLine}| 2 |   Mickey   |     Mouse     |{Environment.NewLine}";
+            var expected = $@"|   | First Name |   Last Name   |{Environment.NewLine}----------------------------------{Environment.NewLine}| 1 |   Heinz    | Dovenschmirtz |{Environment.NewLine}| 2 |   Mickey   |     Mouse     |{Environment.NewLine}";
             Assert.Equal(expected, result);
         }
     }
