5181503c9c95ba6da247259de4831ba6a5832f10
Devedse <davydavidse@gmail.com>
2019-05-28 00:33:06 +0200

    Adding a console menu and a console app to test it

---
+++ DeveCoolLib.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using DeveCoolLib.DeveConsoleMenu;
+using System;
+
+namespace DeveCoolLib.ConsoleApp
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestConsoleMenu();
+        }
+
+        private static void TestConsoleMenu()
+        {
+            var running = true;
+            var menu = new ConsoleMenu(ConsoleMenuType.KeyPress);
+
+            menu.MenuOptions.Add(new ConsoleMenuOption("Do something", () => Console.WriteLine("This is the first option")));
+            menu.MenuOptions.Add(new ConsoleMenuOption("Do something else", () => Console.WriteLine("This is the second option")));
+            menu.MenuOptions.Add(new ConsoleMenuOption("Do something 3", () => Console.WriteLine("This is the third option")));
+            menu.MenuOptions.Add(new ConsoleMenuOption("Exit", () => running = false));
+
+            while (running)
+            {
+                menu.RenderMenu();
+                menu.WaitForResult();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                if (menu.ConsoleMenuType == ConsoleMenuType.KeyPress)
+                {
+                    menu.ConsoleMenuType = ConsoleMenuType.StringInput;
+                }
+                else
+                {
+                    menu.ConsoleMenuType = ConsoleMenuType.KeyPress;
+                }
+            }
+        }
+    }
+}
--- DeveCoolLib/DeveConsoleMenu/ConsoleMenuOption.cs
+++ DeveCoolLib/DeveConsoleMenu/ConsoleMenuOption.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DeveCoolLib.DeveConsoleMenu
 {
     public class ConsoleMenuOption
     {
-        private readonly string _text;
-        private readonly Action _actionToExecute;
+        public string Text { get; }
+        public Action ActionToExecute { get; }
 
         public ConsoleMenuOption(string text, Action actionToExecute)
         {
-            _text = text;
-            _actionToExecute = actionToExecute;
+            Text = text;
+            ActionToExecute = actionToExecute;
         }
     }
 }
