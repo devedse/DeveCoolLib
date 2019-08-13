da002d0c0e95e18279d29c967b3a9dec0a6d4fef
Devedse <davydavidse@gmail.com>
2019-08-13 10:01:38 +0200

    Added logging

---
+++ DeveCoolLib/Directory/FolderHelperMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace DeveCoolLib.Directory
+{
+    public static class FolderHelperMethods
+    {
+        public static Lazy<string> AssemblyDirectory { get; } = new Lazy<string>(() => CreateLocationOfImageProcessorAssemblyDirectory());
+
+        private static string CreateLocationOfImageProcessorAssemblyDirectory()
+        {
+            var assembly = typeof(FolderHelperMethods).GetTypeInfo().Assembly;
+            var assemblyDir = Path.GetDirectoryName(assembly.Location);
+            return assemblyDir;
+        }
+    }
+}
