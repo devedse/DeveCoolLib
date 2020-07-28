ffe6fb9d92295f238f87e6b6d33e7e678f1abb16
Devedse <davydavidse@gmail.com>
2020-07-28 22:21:42 +0200

    Fixed bug in SecondsToString conversion where it wouldnt work for more then 60 hours

--- DeveCoolLib/Conversion/ValuesToStringConverter.cs
+++ DeveCoolLib/Conversion/ValuesToStringConverter.cs
@@ -15,7 +15,7 @@ namespace DeveCoolLib.Conversion
             }
 
             long bytes = Math.Abs(byteCount);
-            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            int place = Math.Min(suf.Length - 1, Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024))));
             double num = Math.Round(bytes / Math.Pow(1024, place), 1);
             var theNumber = Math.Sign(byteCount) * num;
             return $"{theNumber}{suf[place]}";
@@ -30,7 +30,7 @@ namespace DeveCoolLib.Conversion
             }
 
             long bytes = Math.Abs(seconds);
-            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 60)));
+            int place = Math.Min(suf.Length - 1, Convert.ToInt32(Math.Floor(Math.Log(bytes, 60))));
             double num = Math.Round(bytes / Math.Pow(60, place), 1);
             var theNumber = Math.Sign(seconds) * num;
             return $"{theNumber} {suf[place]}{(num == 1 ? "" : "s")}";
