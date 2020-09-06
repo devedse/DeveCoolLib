42bcda078e34c0cbcfb1f515d2fd22511823f876
Devedse <davydavidse@gmail.com>
2020-09-06 19:25:42 +0200

    Improved process runner

--- DeveCoolLib/ProcessAsTask/ProcessRunner.cs
+++ DeveCoolLib/ProcessAsTask/ProcessRunner.cs
@@ -15,6 +15,7 @@ namespace DeveCoolLib.ProcessAsTask
         /// <param name="processStartInfo">The <see cref="T:System.Diagnostics.ProcessStartInfo" /> that contains the information that is used to start the process, including the file name and any command-line arguments.</param>
         /// <param name="standardOutput">List that lines written to standard output by the process will be added to</param>
         /// <param name="standardError">List that lines written to standard error by the process will be added to</param>
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
         /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
         public static async Task<ProcessResults> RunAsync(ProcessStartInfo processStartInfo, List<string> standardOutput, List<string> standardError, CancellationToken cancellationToken, bool forwardLogsToConsole)
         {
@@ -70,18 +71,18 @@ namespace DeveCoolLib.ProcessAsTask
 
                 process.Exited += async (sender, args) =>
                 {
-                // Since the Exited event can happen asynchronously to the output and error events, 
-                // we await the task results for stdout/stderr to ensure they both closed.  We must await
-                // the stdout/stderr tasks instead of just accessing the Result property due to behavior on MacOS.  
-                // For more details, see the PR at https://github.com/jamesmanning/RunProcessAsTask/pull/16/
-                tcs.TrySetResult(
-                        new ProcessResults(
-                            process,
-                            await processStartTime.Task.ConfigureAwait(false),
-                            await standardOutputResults.Task.ConfigureAwait(false),
-                            await standardErrorResults.Task.ConfigureAwait(false)
-                        )
-                    );
+                    // Since the Exited event can happen asynchronously to the output and error events, 
+                    // we await the task results for stdout/stderr to ensure they both closed.  We must await
+                    // the stdout/stderr tasks instead of just accessing the Result property due to behavior on MacOS.  
+                    // For more details, see the PR at https://github.com/jamesmanning/RunProcessAsTask/pull/16/
+                    tcs.TrySetResult(
+                            new ProcessResults(
+                                process,
+                                await processStartTime.Task.ConfigureAwait(false),
+                                await standardOutputResults.Task.ConfigureAwait(false),
+                                await standardErrorResults.Task.ConfigureAwait(false)
+                            )
+                        );
                 };
 
                 using (cancellationToken.Register(
@@ -131,30 +132,34 @@ namespace DeveCoolLib.ProcessAsTask
         /// Runs asynchronous process.
         /// </summary>
         /// <param name="fileName">An application or document which starts the process.</param>
-        public static Task<ProcessResults> RunAsync(string fileName)
-            => RunAsync(new ProcessStartInfo(fileName));
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(string fileName, bool forwardLogsToConsole = false)
+            => RunAsync(new ProcessStartInfo(fileName), forwardLogsToConsole);
 
         /// <summary>
         /// Runs asynchronous process.
         /// </summary>
         /// <param name="fileName">An application or document which starts the process.</param>
-        public static Task<ProcessResults> RunAsync(string fileName, IDictionary<string, string> environmentVariables)
-            => RunAsync(fileName, environmentVariables, CancellationToken.None);
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsync(string fileName, CancellationToken cancellationToken)
-            => RunAsync(new ProcessStartInfo(fileName), cancellationToken);
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(string fileName, IDictionary<string, string> environmentVariables, bool forwardLogsToConsole = false)
+            => RunAsync(fileName, environmentVariables, CancellationToken.None, forwardLogsToConsole);
 
         /// <summary>
         /// Runs asynchronous process.
         /// </summary>
         /// <param name="fileName">An application or document which starts the process.</param>
         /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsync(string fileName, IDictionary<string, string> environmentVariables, CancellationToken cancellationToken)
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(string fileName, CancellationToken cancellationToken, bool forwardLogsToConsole = false)
+            => RunAsync(new ProcessStartInfo(fileName), cancellationToken, forwardLogsToConsole);
+
+        /// <summary>
+        /// Runs asynchronous process.
+        /// </summary>
+        /// <param name="fileName">An application or document which starts the process.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(string fileName, IDictionary<string, string> environmentVariables, CancellationToken cancellationToken, bool forwardLogsToConsole = false)
         {
             var psi = new ProcessStartInfo(fileName);
             foreach (var envVariable in environmentVariables)
@@ -162,7 +167,7 @@ namespace DeveCoolLib.ProcessAsTask
                 psi.EnvironmentVariables.Add(envVariable.Key, envVariable.Value);
             }
 
-            return RunAsync(psi, cancellationToken);
+            return RunAsync(psi, cancellationToken, forwardLogsToConsole);
         }
 
         /// <summary>
@@ -170,8 +175,9 @@ namespace DeveCoolLib.ProcessAsTask
         /// </summary>
         /// <param name="fileName">An application or document which starts the process.</param>
         /// <param name="arguments">Command-line arguments to pass to the application when the process starts.</param>
-        public static Task<ProcessResults> RunAsync(string fileName, string arguments)
-            => RunAsync(new ProcessStartInfo(fileName, arguments));
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(string fileName, string arguments, bool forwardLogsToConsole = false)
+            => RunAsync(new ProcessStartInfo(fileName, arguments), forwardLogsToConsole);
 
         /// <summary>
         /// Runs asynchronous process.
@@ -179,8 +185,9 @@ namespace DeveCoolLib.ProcessAsTask
         /// <param name="fileName">An application or document which starts the process.</param>
         /// <param name="arguments">Command-line arguments to pass to the application when the process starts.</param>
         /// <param name="environmentVariables">Environment variables to use when the process starts.</param>
-        public static Task<ProcessResults> RunAsync(string fileName, string arguments, IDictionary<string, string> environmentVariables)
-            => RunAsync(fileName, arguments, environmentVariables, CancellationToken.None);
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(string fileName, string arguments, IDictionary<string, string> environmentVariables, bool forwardLogsToConsole = false)
+            => RunAsync(fileName, arguments, environmentVariables, CancellationToken.None, forwardLogsToConsole);
 
         /// <summary>
         /// Runs asynchronous process.
@@ -188,8 +195,9 @@ namespace DeveCoolLib.ProcessAsTask
         /// <param name="fileName">An application or document which starts the process.</param>
         /// <param name="arguments">Command-line arguments to pass to the application when the process starts.</param>
         /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsync(string fileName, string arguments, CancellationToken cancellationToken)
-            => RunAsync(new ProcessStartInfo(fileName, arguments), cancellationToken);
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(string fileName, string arguments, CancellationToken cancellationToken, bool forwardLogsToConsole = false)
+            => RunAsync(new ProcessStartInfo(fileName, arguments), cancellationToken, forwardLogsToConsole);
 
         /// <summary>
         /// Runs asynchronous process.
@@ -198,7 +206,8 @@ namespace DeveCoolLib.ProcessAsTask
         /// <param name="arguments">Command-line arguments to pass to the application when the process starts.</param>
         /// <param name="environmentVariables">Environment variables to use when the process starts.</param>
         /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsync(string fileName, string arguments, IDictionary<string, string> environmentVariables, CancellationToken cancellationToken)
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(string fileName, string arguments, IDictionary<string, string> environmentVariables, CancellationToken cancellationToken, bool forwardLogsToConsole = false)
         {
             var psi = new ProcessStartInfo(fileName, arguments);
             foreach (var envVariable in environmentVariables)
@@ -206,123 +215,25 @@ namespace DeveCoolLib.ProcessAsTask
                 psi.EnvironmentVariables.Add(envVariable.Key, envVariable.Value);
             }
 
-            return RunAsync(psi, cancellationToken);
-        }
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="processStartInfo">The <see cref="T:System.Diagnostics.ProcessStartInfo" /> that contains the information that is used to start the process, including the file name and any command-line arguments.</param>
-        public static Task<ProcessResults> RunAsync(ProcessStartInfo processStartInfo)
-            => RunAsync(processStartInfo, CancellationToken.None);
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="processStartInfo">The <see cref="T:System.Diagnostics.ProcessStartInfo" /> that contains the information that is used to start the process, including the file name and any command-line arguments.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsync(ProcessStartInfo processStartInfo, CancellationToken cancellationToken)
-            => RunAsync(processStartInfo, new List<string>(), new List<string>(), cancellationToken, false);
-
-
-
-
-
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(string fileName)
-            => RunAsyncAndLogToConsole(new ProcessStartInfo(fileName));
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(string fileName, IDictionary<string, string> environmentVariables)
-            => RunAsyncAndLogToConsole(fileName, environmentVariables, CancellationToken.None);
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(string fileName, CancellationToken cancellationToken)
-            => RunAsyncAndLogToConsole(new ProcessStartInfo(fileName), cancellationToken);
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(string fileName, IDictionary<string, string> environmentVariables, CancellationToken cancellationToken)
-        {
-            var psi = new ProcessStartInfo(fileName);
-            foreach (var envVariable in environmentVariables)
-            {
-                psi.EnvironmentVariables.Add(envVariable.Key, envVariable.Value);
-            }
-
-            return RunAsyncAndLogToConsole(psi, cancellationToken);
+            return RunAsync(psi, cancellationToken, forwardLogsToConsole);
         }
 
         /// <summary>
         /// Runs asynchronous process.
         /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        /// <param name="arguments">Command-line arguments to pass to the application when the process starts.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(string fileName, string arguments)
-            => RunAsyncAndLogToConsole(new ProcessStartInfo(fileName, arguments));
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        /// <param name="arguments">Command-line arguments to pass to the application when the process starts.</param>
-        /// <param name="environmentVariables">Environment variables to use when the process starts.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(string fileName, string arguments, IDictionary<string, string> environmentVariables)
-            => RunAsyncAndLogToConsole(fileName, arguments, environmentVariables, CancellationToken.None);
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        /// <param name="arguments">Command-line arguments to pass to the application when the process starts.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(string fileName, string arguments, CancellationToken cancellationToken)
-            => RunAsyncAndLogToConsole(new ProcessStartInfo(fileName, arguments), cancellationToken);
-
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
-        /// <param name="fileName">An application or document which starts the process.</param>
-        /// <param name="arguments">Command-line arguments to pass to the application when the process starts.</param>
-        /// <param name="environmentVariables">Environment variables to use when the process starts.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(string fileName, string arguments, IDictionary<string, string> environmentVariables, CancellationToken cancellationToken)
-        {
-            var psi = new ProcessStartInfo(fileName, arguments);
-            foreach (var envVariable in environmentVariables)
-            {
-                psi.EnvironmentVariables.Add(envVariable.Key, envVariable.Value);
-            }
-
-            return RunAsyncAndLogToConsole(psi, cancellationToken);
-        }
-        /// <summary>
-        /// Runs asynchronous process.
-        /// </summary>
         /// <param name="processStartInfo">The <see cref="T:System.Diagnostics.ProcessStartInfo" /> that contains the information that is used to start the process, including the file name and any command-line arguments.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(ProcessStartInfo processStartInfo)
-            => RunAsyncAndLogToConsole(processStartInfo, CancellationToken.None);
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(ProcessStartInfo processStartInfo, bool forwardLogsToConsole = false)
+            => RunAsync(processStartInfo, CancellationToken.None, forwardLogsToConsole);
 
         /// <summary>
         /// Runs asynchronous process.
         /// </summary>
         /// <param name="processStartInfo">The <see cref="T:System.Diagnostics.ProcessStartInfo" /> that contains the information that is used to start the process, including the file name and any command-line arguments.</param>
         /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
-        public static Task<ProcessResults> RunAsyncAndLogToConsole(ProcessStartInfo processStartInfo, CancellationToken cancellationToken)
-            => RunAsync(processStartInfo, new List<string>(), new List<string>(), cancellationToken, true);
+        /// <param name="forwardLogsToConsole">Forwards the process logs to the console</param>
+        public static Task<ProcessResults> RunAsync(ProcessStartInfo processStartInfo, CancellationToken cancellationToken, bool forwardLogsToConsole = false)
+            => RunAsync(processStartInfo, new List<string>(), new List<string>(), cancellationToken, forwardLogsToConsole);
     }
 }
 
